5ea94fd81ff1963b142f6975520da0678730e8ab
Roshan-Shah-7 <roshanshah12113@gmail.com>
2024-08-20 10:13:35 +0545

    Added question 1

---
+++ Question1.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Question1
+{
+    class Q1
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Welcome {name}");
+            Console.ReadKey();
+        }
+    }
+}
